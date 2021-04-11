468565d6113cb365f46f458ec93e5113b232f994
sparuchuri19 <sp30@illinois.edu>
2021-04-11 14:28:59 +0530

    Update process_data.py
    Added smote+tomek augmentation

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from imblearn.combine import SMOTETomek
 
 class CustomDataset(Dataset):
     
@@ -23,13 +24,18 @@ def load_data(batch_size = 128):
     df_train = df_train.sample(frac=1)
     df_test = pd.read_csv("input/mitbih_test.csv", header=None)
 
-    Y = np.array(df_train[187].values)
-    X = np.array(df_train[list(range(187))].values)[..., np.newaxis]
+    Y = np.array(df_train[187].values).astype(int)
+    X = np.array(df_train[list(range(187))].values)
 
     Y_test = np.array(df_test[187].values)
     X_test = np.array(df_test[list(range(187))].values)[..., np.newaxis]
+	
+	#Smote for data augmentation
+	sm = SMOTETomek()
+	X_sm, y_sm = sm.fit_resample(X,y)  
+    X_sm = X_sm[..., np.newaxis]
 
-    train_dataset = CustomDataset(X, Y)
+    train_dataset = CustomDataset(X_sm, Y_sm)
     val_dataset = CustomDataset(X_test, Y_test)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
